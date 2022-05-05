bc1c5fb4e1a68797f90dba6613d5e816237283b7
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-04 23:52:08 -0300

    :wrench: feat: create use case to create feedback

---
+++ server/src/api/useCases/CreateFeedback/CreateFeedbackUseCase.ts
@@ -0,0 +1,34 @@
+import { INodemailerProvider } from "../../providers/INodemailerProvider";
+import { IFeedbacksRepository } from "../../repositories/IFeedbacksRepository";
+import { ICreateFeedbackDTO } from "./CreateFeedbackDTO";
+
+export class CreateFeedbackUseCase {
+    constructor(
+        private feedbacksRepository: IFeedbacksRepository,
+        private nodemailerProvider: INodemailerProvider
+    ) {}
+
+    async execute({ type, comment, screenshot }: ICreateFeedbackDTO) {
+
+        if(!type) throw new Error("Type is required")
+        if(!comment) throw new Error("Comment is required")
+        if (screenshot && !screenshot.startsWith("data:image/png;base64"))
+            throw new Error("Invalid screenshot format.");
+
+        await this.feedbacksRepository.create({
+            type,
+            comment,
+            screenshot,
+        });
+
+        await this.nodemailerProvider.sendMail({
+            subject: "Novo feedback",
+            body: [
+                `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+                `<p>Tipo do feedback: ${type}</p>`,
+                `<p>Comentário: ${comment}</p>`,
+                `</div>`,
+            ].join("\n"),
+        });
+    }
+}
