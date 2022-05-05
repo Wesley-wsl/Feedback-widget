0be373a011a3b5d02fa7a3f3cda5e117b20fc11c
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-05 00:06:30 -0300

    :wrench: feat: allow environment variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { feedbackRouter } from "./api/routes/feedbacks.routes";
 import cors from "cors";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
@@ -8,4 +11,4 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/v1/feedbacks", feedbackRouter);
 
-app.listen(3333, () => console.log("Server is running."));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running."));
