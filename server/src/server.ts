1d3e0519874e767a59702e763404ce3876877e76
Wesley Westelley <wesleywestelley@gmail.com>
2022-05-08 18:23:22 -0300

    :wrench: feat: add handle error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
+import "express-async-errors";
 import express from "express";
 import { feedbackRouter } from "./api/routes/feedbacks.routes";
 import cors from "cors";
 import { config } from "dotenv";
+import { errorHandler } from "./api/middlewares/errorHandler";
 
 config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/api/v1/feedbacks", feedbackRouter);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333, () => console.log("Server is running."));
